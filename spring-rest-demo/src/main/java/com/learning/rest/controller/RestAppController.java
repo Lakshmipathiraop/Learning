e4ba22c4dc6009e5aeb4777bd2d3a9eb5c1b236a
Lucky <Lakshmipathirao.pedamallu@gmail.com>
2020-11-23 20:09:06 +0530

    Changed sysout

--- spring-rest-demo/src/main/java/com/learning/rest/controller/RestAppController.java
+++ spring-rest-demo/src/main/java/com/learning/rest/controller/RestAppController.java
@@ -10,7 +10,7 @@ public class RestAppController {
 	
 	@GetMapping("/hello")
 	public static String sayHello() {
-		System.out.println("THis is nw commit done in master branch which is not in feature branch");
+		System.out.println("THis is new commit done in master branch which is not in feature branch");
 		System.out.println("This is the commit that iam doing in feature branch after rebase");
 		return "Hello Lucky,Welcome to the Rest world";
 		
